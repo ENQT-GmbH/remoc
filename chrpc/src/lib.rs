ce6b4ed17fa4b9d2b19fc0ebb7cab9f32387259f
Sebastian Urban <su@enqt.de>
2020-02-13 14:47:25 +0100

    RPC refactoring.

---
+++ chrpc/src/lib.rs
@@ -0,0 +1,205 @@
+use std::pin::Pin;
+use std::fmt;
+use std::error::Error;
+use futures::sink::Sink;
+use futures::stream::{Stream, StreamExt};
+use futures::task::Poll;
+use futures::task::Context;
+use pin_project::pin_project;
+use std::marker::PhantomData;
+use pin_project::pinned_drop;
+
+use serde::{Serialize, de::DeserializeOwned};
+use chmux::{SendError, ReceiveError, ConnectError};
+
+pub use chrpc_macro::service;
+
+
+/// An error occured during an RPC call.
+#[derive(Debug)]
+pub enum CallError {
+    /// Connection has been rejected by server.
+    Rejected,
+    /// A multiplexer error has occured or it has been terminated.
+    MultiplexerError,
+    /// Error serializing the service request.
+    SerializationError(Box<dyn Error + Send + 'static>),  
+    /// A deserialization error occured.
+    DeserializationError(Box<dyn Error + Send + 'static>), 
+}
+
+impl fmt::Display for CallError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Rejected => write!(f, "Connection has been rejected by server."),
+            Self::MultiplexerError => write!(f, "A multiplexer error has occured or it has been terminated."),
+            Self::SerializationError(err) => write!(f, "A serialization error occured: {}", err),            
+            Self::DeserializationError(err) => write!(f, "A deserialization error occured: {}", err),
+        }
+    }
+}
+
+impl Error for CallError {}
+
+impl From<ConnectError> for CallError {
+    fn from(err: ConnectError) -> Self {
+        match err {
+            ConnectError::MultiplexerError => Self::MultiplexerError,
+            ConnectError::Rejected => Self::Rejected,
+            ConnectError::SerializationError (err) => Self::SerializationError(err)
+        }
+    }
+}
+
+impl From<ReceiveError> for CallError {
+    fn from(err: ReceiveError) -> Self {
+        match err {
+            ReceiveError::MultiplexerError => Self::MultiplexerError,
+            ReceiveError::DeserializationError (err) => Self::DeserializationError(err)
+        }
+    }
+}
+
+/// RPC sender.
+#[pin_project(PinnedDrop)]
+pub struct Sender<'a, T> 
+{
+    #[pin]
+    sender: chmux::Sender<T>,
+    _ghost: PhantomData<&'a ()>,
+}
+
+#[pinned_drop]
+impl<'a, T> PinnedDrop for Sender<'a, T> {
+    fn drop(self: Pin<&mut Self>) {
+        // Drop implementation must be present to make reference guard work.
+    }
+}
+
+impl<'a, T> Sink<T> for Sender<'a, T> 
+where
+    T: Serialize,
+{
+    type Error = SendError;
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.project().sender.poll_ready(cx)
+    }
+    fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
+        self.project().sender.start_send(item)
+    }
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.project().sender.poll_flush(cx)
+    }
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.project().sender.poll_close(cx)
+    }    
+}
+
+impl<'a, T> Sender<'a, T>
+where
+    T: Serialize, {
+    #[doc(hidden)]
+    pub fn new(sender: chmux::Sender<T>) -> Self {
+        Self {
+            sender,
+            _ghost: PhantomData
+        }
+    }
+}
+
+/// RPC receiver.
+#[pin_project(PinnedDrop)]
+pub struct Receiver<'a, T> {
+    #[pin]
+    receiver: chmux::Receiver<T>,
+    _ghost: PhantomData<&'a ()>,
+}
+
+#[pinned_drop]
+impl<'a, T> PinnedDrop for Receiver<'a, T> {
+    fn drop(self: Pin<&mut Self>) {
+        // Drop implementation must be present to make reference guard work.
+    }
+}
+
+impl<'a, T> Stream for Receiver<'a, T> 
+where T: DeserializeOwned {
+    type Item = Result<T, ReceiveError>;
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        self.project().receiver.poll_next(cx)
+    }
+}
+
+impl<'a, T> Receiver<'a, T> 
+where T: DeserializeOwned {
+    #[doc(hidden)]
+    pub fn new(receiver: chmux::Receiver<T>) -> Self {
+        Self {
+            receiver,
+            _ghost: PhantomData,
+        }
+    }
+
+    /// Prevents the remote endpoint from sending new messages into this channel while
+    /// allowing in-flight messages to be received.     
+    pub async fn close(&mut self) {
+        self.receiver.close().await
+    }
+}
+
+/// A sink that sends items to an RPC method.
+/// 
+/// It can be closed, returning the value of the RPC method.
+pub struct SendingCall<'a, S, T> {
+    sender: Option<Pin<Box<chmux::Sender<S>>>>,
+    receiver: Pin<Box<chmux::Receiver<T>>>,
+    _ghost: PhantomData<&'a ()>
+}
+
+impl<'a, S, T> Drop for SendingCall<'a, S, T> {
+    fn drop(&mut self) {
+        // Drop implementation must be present to make reference guard work.
+    }
+}
+
+impl<'a, S, T> Sink<S> for SendingCall<'a, S, T> 
+where S: Serialize, T: DeserializeOwned
+{
+    type Error = SendError;
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.sender.as_mut().unwrap().as_mut().poll_ready(cx)
+    }
+    fn start_send(mut self: Pin<&mut Self>, item: S) -> Result<(), Self::Error> {
+        self.sender.as_mut().unwrap().as_mut().start_send(item)
+    }
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.sender.as_mut().unwrap().as_mut().poll_flush(cx)
+    }
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        self.sender.as_mut().unwrap().as_mut().poll_close(cx)
+    }    
+}
+
+impl<'a, S, T> SendingCall<'a, S, T> 
+where S: Serialize, T: DeserializeOwned
+{
+    #[doc(hidden)]
+    pub fn new(sender: chmux::Sender<S>, receiver: chmux::Receiver<T>) -> Self {
+        Self {
+            sender: Some(Box::pin(sender)),
+            receiver: Box::pin(receiver),
+            _ghost: PhantomData
+        }
+    }
+    
+    /// Closes the sink and returns a Future resolving to the return value of the 
+    /// RPC method.    
+    pub async fn finish(mut self) -> Result<T, chmux::ReceiveError> {
+        self.sender = None;
+        match self.receiver.next().await {
+            Some(ret) => ret,
+            None => Err(chmux::ReceiveError::MultiplexerError)
+        }
+    }
+}
+
---
+++ chrpc/tests/service_test.rs
@@ -0,0 +1,158 @@
+use std::net::Ipv4Addr;
+use chmux;
+use chmux::codecs::json::{JsonContentCodec, JsonTransportCodec};
+use chrpc::service;
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
+use tokio::io::split;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_util::codec::length_delimited::LengthDelimitedCodec;
+use tokio_util::codec::{FramedRead, FramedWrite};
+
+#[service]
+pub trait TestTrait {
+    async fn method_one(&self, param1: u32, param2: u64, tx: chmux::Sender<String>);
+    async fn method_two(&self, param1: String) -> usize;
+    async fn method_three(&mut self, param1: f32, param2: f64, value: u32, rx: chmux::Receiver<u16>) -> String;
+}
+
+struct TestObj {
+    value: u32
+}
+
+impl TestObj {
+    pub fn new() -> TestObj {
+        TestObj {
+            value: 123
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl TestTrait for TestObj {
+    async fn method_one(&self, param1: u32, param2: u64, mut tx: chmux::Sender<String>) {
+        tx.send(format!("param1={}, param2={}, value={}", param1, param2, self.value)).await.unwrap();
+    }
+
+    async fn method_two(&self, param1: String) -> usize {
+        param1.len()
+    }
+
+    async fn method_three(&mut self, param1: f32, param2: f64, value: u32, mut rx: chmux::Receiver<u16>) -> String {
+        self.value = value;
+        let mut data = format!("param1={}, param2={}, value={}\n", param1, param2, value);
+        println!("server: method_three: {}", &data);
+        loop {
+            match rx.next().await {
+                Some(Ok(x)) => {
+                    data.push_str(&format!("rx: {}\n", x));
+                    println!("server: method_three: rx: {}", x);
+                },
+                _ => break
+            }
+        }
+        println!("server: method_three finished: {}", &data);
+        data
+    }
+}
+
+async fn server_part() {
+    println!("Server waiting for TCP connection...");
+    let mut listener = TcpListener::bind((Ipv4Addr::new(127, 0, 0, 1), 9876)).await.unwrap();
+    let (socket, _) = listener.accept().await.unwrap();
+    let (socket_rx, socket_tx) = split(socket);
+    let framed_tx = FramedWrite::new(socket_tx, LengthDelimitedCodec::new());
+    let framed_rx = FramedRead::new(socket_rx, LengthDelimitedCodec::new());
+    let framed_rx = framed_rx.map(|data| data.map(|b| b.freeze()));
+
+    let mux_cfg = chmux::Cfg::default();
+    let content_codec = JsonContentCodec::new();
+    let transport_codec = JsonTransportCodec::new();
+
+    let (mux, _, server) =
+        chmux::Multiplexer::new(&mux_cfg, &content_codec, &transport_codec, framed_tx, framed_rx);
+
+    let mux_run = tokio::spawn(async move { mux.run().await.unwrap() });
+
+    let obj = TestObj::new();
+    println!("Server serving...");
+    obj.serve(server).await.unwrap();
+    println!("Serving ended.");
+
+    println!("Waiting for server mux to terminate...");
+    mux_run.await.unwrap();
+    println!("server mux ended.");
+}
+
+async fn client_part() {
+    println!("Client making TCP connection...");
+    let socket = TcpStream::connect((Ipv4Addr::new(127, 0, 0, 1), 9876)).await.unwrap();
+    let (socket_rx, socket_tx) = split(socket);
+    let framed_tx = FramedWrite::new(socket_tx, LengthDelimitedCodec::new());
+    let framed_rx = FramedRead::new(socket_rx, LengthDelimitedCodec::new());
+    let framed_rx = framed_rx.map(|data| data.map(|b| b.freeze()));
+
+    let mux_cfg = chmux::Cfg::default();
+    let content_codec = JsonContentCodec::new();
+    let transport_codec = JsonTransportCodec::new();
+
+    let (mux, client, _) =
+        chmux::Multiplexer::new(&mux_cfg, &content_codec, &transport_codec, framed_tx, framed_rx);
+
+    let mux_run = tokio::spawn(async move { mux.run().await.unwrap() });
+
+    println!("Client making requests...");
+    let mut obj_client = TestTraitClient::bind(client);
+    println!("Client ended.");
+
+    println!("Calling method_one..");
+    let mut rx_one = obj_client.method_one(111, 222).await.unwrap();
+    loop {
+        match rx_one.next().await {
+            Some(msg) => println!("rx_one: {}", msg.unwrap()),
+            None => break
+        }
+    }
+    
+    println!("Calling method_two...");
+    let res_two = obj_client.method_two("1234567".to_string()).await.unwrap();
+    println!("Method two: {}", res_two);
+
+    println!("Dropping rx_one...");
+    drop(rx_one);    
+
+    println!("Calling method three...");
+    let mut tx_three = obj_client.method_three(11.1, 22.22, 32).await.unwrap();
+    println!("Method three send 11");
+    tx_three.send(11).await.unwrap();
+    println!("Method three send 22");
+    tx_three.send(22).await.unwrap();
+    println!("Method three send 33");
+    tx_three.send(33).await.unwrap();
+    println!("Method three finish");
+    let res_three = tx_three.finish().await.unwrap();
+    println!("Method three: {}", res_three);
+
+    //rx_one.next().await;
+
+    println!("Dropping client...");
+    drop(obj_client);
+    println!("Client dropped.");
+
+    println!("Waiting for client mux to terminate...");
+    mux_run.await.unwrap();
+    println!("client mux ended.");    
+}
+
+#[tokio::test]
+async fn test1() {
+    env_logger::init();
+
+    let server = tokio::spawn(server_part());
+    let client = tokio::spawn(client_part());
+
+    println!("Waiting for client to terminate...");
+    client.await.unwrap();
+    println!("Waiting for server to terminate...");
+    server.await.unwrap();
+}
