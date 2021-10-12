ba357a2f927d47f7e23d1cf9bdce12a891f75829
Sebastian Urban <surban@surban.net>
2021-10-12 14:46:14 +0200

    mpsc forwarding test

--- remoc/tests/rsync/mpsc.rs
+++ remoc/tests/rsync/mpsc.rs
@@ -82,3 +82,58 @@ async fn multiple() {
 
     future::try_join_all(tasks).await.unwrap();
 }
+
+#[tokio::test]
+async fn forward() {
+    crate::init();
+    let ((mut a0_tx, _), (_, mut b0_rx)) = loop_channel::<mpsc::Sender<i16, JsonCodec, 16>>().await;
+    let ((mut a1_tx, _), (_, mut b1_rx)) = loop_channel::<mpsc::Sender<i16, JsonCodec, 16>>().await;
+    let ((mut a2_tx, _), (_, mut b2_rx)) = loop_channel::<mpsc::Receiver<i16, JsonCodec, 16>>().await;
+    let ((mut a3_tx, _), (_, mut b3_rx)) = loop_channel::<mpsc::Receiver<i16, JsonCodec, 16>>().await;
+
+    let (tx, rx) = mpsc::channel::<_, _, 16, 16>(16);
+
+    println!("Sender 0");
+    a0_tx.send(tx).await.unwrap();
+    let tx = b0_rx.recv().await.unwrap().unwrap();
+    println!("Sender 1");
+    a1_tx.send(tx).await.unwrap();
+    let tx = b1_rx.recv().await.unwrap().unwrap();
+    println!("Sender 0");
+    a0_tx.send(tx).await.unwrap();
+    let tx = b0_rx.recv().await.unwrap().unwrap();
+
+    println!("Receiver 2");
+    a2_tx.send(rx).await.unwrap();
+    let rx = b2_rx.recv().await.unwrap().unwrap();
+    println!("Receiver 3");
+    a3_tx.send(rx).await.unwrap();
+    let rx = b3_rx.recv().await.unwrap().unwrap();
+    println!("Receiver 3");
+    a3_tx.send(rx).await.unwrap();
+    let mut rx = b3_rx.recv().await.unwrap().unwrap();
+
+    for i in 1..1024 {
+        println!("Sending {}", i);
+        let tx = tx.clone();
+        tx.send(i).await.unwrap();
+        let r = rx.recv().await.unwrap().unwrap();
+        println!("Received {}", r);
+        assert_eq!(i, r, "send/receive mismatch");
+    }
+
+    println!("Verifying that channel is open");
+    assert!(!tx.is_closed());
+    rx.close();
+
+    println!("Closing channel");
+    tx.closed().await;
+    assert!(tx.is_closed());
+
+    println!("Trying send after close");
+    match tx.send(0).await {
+        Ok(_) => panic!("send succeeded after close"),
+        Err(err) if err.is_closed() => (),
+        Err(_) => panic!("wrong error after close"),
+    }
+}
