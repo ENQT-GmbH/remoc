b9e40482a013caf46de1bf64d3f9dd3cafa6f058
Sebastian Urban <surban@surban.net>
2021-10-19 10:15:05 +0200

    prelude

---
+++ remoc/src/prelude.rs
@@ -0,0 +1,31 @@
+//! Convenience re-export of common members.
+//!
+//! Like the standard library's prelude, this module simplifies importing of common items.
+//! Unlike the standard prelude, the contents of this module must be imported manually.
+//!
+//! ```
+//! use remoc::prelude::*;
+//! ```
+//!
+
+pub use crate::chmux;
+
+#[cfg(feature = "rch")]
+pub use crate::rch;
+
+#[cfg(feature = "rch")]
+#[doc(no_inline)]
+pub use crate::RemoteSend;
+
+#[cfg(feature = "rfn")]
+pub use crate::rfn;
+
+#[cfg(feature = "robj")]
+pub use crate::robj;
+
+#[cfg(feature = "rtc")]
+pub use crate::rtc;
+
+#[cfg(feature = "rtc")]
+#[doc(no_inline)]
+pub use crate::rtc::{Server, ServerRef, ServerRefMut, ServerShared, ServerSharedMut};
